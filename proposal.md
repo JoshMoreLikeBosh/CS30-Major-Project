ad197ac4e90c2f8bf0c7ed03fdb0c53cae292175
Bosh <126823337+JoshMoreLikeBosh@users.noreply.github.com>
2023-04-21 15:20:08 -0600

    Worked on Camera and make starting plane

--- proposal.md
+++ proposal.md
@@ -2,19 +2,28 @@
 
 ## Project Description
 
-fps shooting range, 
+fps shooting range, laser guns and stuff 
 
 ## Need to have List
 
-- thing 1
-- thing 2
-- thing 3
+- PHYSICS
+- COLLISION
+- Range
+- targets
+- at least 1 weapon
+- movement (includes jumping)
+- camera
+- crosshair
+- sound effects and bg music
 
 ## Nice to have List
 
-- thing 1
-- thing 2
-- thing 3
+- moving targets
+- multiple weapons
+- parkour elements / obstacle course
+- sliding and super jumping
+- wall jumps
+- 
 
 ## Project Stuff
 
--- sketch.js
+++ sketch.js
@@ -5,13 +5,99 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let cam;
+let images;
+let startingCamPosX = 200;
+let startingCamPosY = -250;
+let startingCamPosZ = 2000;
+let rover;
+
+function preload() {
+  images = [];
+  images.push(loadImage("grid.png"));
+  images.push(loadImage("gun1.png"));
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER);
-}
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  //cam = createCamera();
+  angleMode(DEGREES);
+  
+  //cam.setPosition(startingCamPosX, startingCamPosY, startingCamPosZ);
+  rover = createRoverCam();
+  rover.usePointerLock();    // optional; default is keyboard control only
+  rover.setState({           // optional
+    position: [-400,-200,-200],
+    rotation: [0.4,0.3,0],
+    sensitivity: 0.001,
+    speed: 0.5
+    fov: 1
+  });
+  }
+
 
 function draw() {
-  background(220);
-  rect(mouseX, mouseY, 50, 50);
+  background("black");
+  createGround();
+  CameraControls();
 }
+
+function createGround() {
+  normalMaterial();
+  push();
+  texture(images[0]);
+  rotateX(90);
+  plane(500);
+  pop();
+}
+
+function CameraControls() {
+
+
+
+
+  push();
+  texture(images[1]);
+  plane(windowWidth, windowHeight);
+  pop();
+}
+
+
+// if (keyIsDown(39)) {
+//   cam.pan(-3);
+// }
+// // right arrow pans cam right
+// if (keyIsDown(37)) {
+//   cam.pan(3);
+// }
+// //down arrow tilts cam down
+// if (keyIsDown(38)) {
+//   cam.tilt(-3);
+// }
+// //  up arrow tilts cam up
+// if (keyIsDown(40)) {
+//   cam.tilt(3);
+// }
+// if (keyIsDown(81)) {
+//   cam.move(0, 0, 40);
+// }
+// //e zoom out
+// if (keyIsDown(69)) {
+//   cam.move(0, 0, -40);
+// }
+// //a moves cam left
+// if (keyIsDown(65)) {
+//   cam.move(-15, 0, 0);
+// }
+// //d moves cam right
+// if (keyIsDown(68)) {
+//   cam.move(15, 0, 0);
+// }
+// // w moves cam up
+// if (keyIsDown(87)) {
+//   cam.move(0, -15, 0);
+// }
+// // s moves can down
+// if (keyIsDown(83)) {
+//   cam.move(0, 15, 0);
+// }
