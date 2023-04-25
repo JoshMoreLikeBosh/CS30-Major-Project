ab08d59308500ec80437a85dcad9cde377f08f8c
Bosh <126823337+JoshMoreLikeBosh@users.noreply.github.com>
2023-04-25 15:19:32 -0600

    gravity

--- sketch.js
+++ sketch.js
@@ -4,17 +4,57 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-let sprite;
+let yVel;
+let accel;
+let velocity;
+let mass;
+let groundTop;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 
-  sprite = new Sprite();
-  sprite.width = 50;
-  sprite.height = 50;
+  yVel = 0;
+  velocity = 0;
+  mass = 100;
+
+  accel = mass * 0.1;
+}
+function draw() {
+  background(50);
+  fill(255,0,0);
+
+  // createGround();
+  createPlayer();
+
 }
 
-function draw() {
-  clear();
+function createPlayer() {
+  push();
+  rectMode(CENTER);
+  velocity += accel;
+  yVel += velocity;
+  rect(mouseX,yVel, mass,mass);
+
+  if (yVel > height - mass/2) {
+    velocity *= -0.3;
+    yVel = height - mass/2;
+  }
+  pop();
+}
+
+function createGround() {
+  push();
+  fill("purple");
+  rect(0, windowHeight-50, windowWidth, 50);
+  pop();
+}
+
+function gravity() {
+
+}
+
+function mousePressed() {
+  yVel = 0;
+  velocity = 0;
 }
