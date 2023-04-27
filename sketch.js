85070fba965a5a65db702f060cb99e82617966a1
Bosh <126823337+JoshMoreLikeBosh@users.noreply.github.com>
2023-04-27 13:04:45 -0600

    fixed the jumping and now trying to p5play for player instead of regular rect

--- sketch.js
+++ sketch.js
@@ -12,14 +12,17 @@ let groundTop;
 let images;
 let img;
 let movement = 0;
+let groundHeight;
+let walls;
+let sprite;
 
 function preload() {
-  let images = [];
-
+  //let images = [];
+  img = loadImage("walkright.gif");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  new Canvas(windowWidth, windowHeight);
   noStroke();
 
   yVel = 0;
@@ -27,15 +30,24 @@ function setup() {
   mass = 100;
 
   accel = mass * 0.1;
+
+  groundHeight = 50;
+
+  sprite = new Sprite();
+  sprite.width = mass;
+  sprite.height = mass;
+  sprite.x = movement;
+  sprite.y = yVel;
 }
+
+
 function draw() {
   background(50);
-  fill(255,0,0);
+  //fill(255,0,0);
 
   createGround();
-  createPlayer();
+  //createPlayer();
   playerControls();
-
 }
 
 
@@ -43,18 +55,18 @@ function draw() {
 function createGround() {
   push();
   fill("purple");
-  rect(0, windowHeight-50, windowWidth, 50);
+  rect(0, windowHeight-50, windowWidth, groundHeight);
   pop();
 }
 
 
-function mousePressed() {
-  push();
-  yVel = 0;
-  velocity = 0;
-  mouseX===mouseX;
-  pop();
-}
+// function mousePressed() {
+//   push();
+//   yVel = 0;
+//   velocity = 0;
+//   mouseX===mouseX;
+//   pop();
+// }
 
 function playerControls() {
   if (keyIsDown(68)) {
@@ -64,26 +76,38 @@ function playerControls() {
     movement -= 15;
   }
   if (keyIsDown(32)) {
-    yVel = 300;
+    yVel -= mass/2;
     if (keyIsDown(65)) {
-      movement -= 30;
+      movement -= 17;
     }
     if (keyIsDown(68)) {
-      movement += 30;
+      movement += 17;
     }
   }
 }
 
-function createPlayer() {
-  push();
-  rectMode(CENTER);
-  velocity += accel;
-  yVel += velocity;
-  rect(movement,yVel, mass,mass);
 
-  if (yVel > height - mass/2) {
-    velocity *= -0.3;
-    yVel = height - mass/2;
-  }
-  pop();
-}
+
+
+
+
+// function createPlayer() {
+//   push();
+//   rectMode(CENTER);
+//   velocity += accel;
+//   yVel += velocity;
+//   push();
+//   sprite = new Sprite();
+//   sprite.width = mass;
+//   sprite.height = mass;
+//   sprite.x = movement;
+//   sprite.y = yVel;
+//   //texture(img);
+//   //rect(movement,yVel, mass,mass);
+//   pop();
+//   if (yVel > height - mass/2) {
+//     velocity *= -0.3;
+//     yVel = height - mass/2;
+//   }
+//   pop();
+// }
