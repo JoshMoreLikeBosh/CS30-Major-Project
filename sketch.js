25fdb2f534a4f8a32795cb2759c677f1ad209bef
Bosh <126823337+JoshMoreLikeBosh@users.noreply.github.com>
2023-04-28 15:19:32 -0600

    i swapped to p5 play and have a way better platformer

--- sketch.js
+++ sketch.js
@@ -4,110 +4,41 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let yVel;
-let accel;
-let velocity;
-let mass;
-let groundTop;
-let images;
-let img;
-let movement = 0;
-let groundHeight;
-let walls;
-let sprite;
 
-function preload() {
-  //let images = [];
-  img = loadImage("walkright.gif");
-}
+let player, floor, floor2, floor3;
+let floorHeight = 0;
 
 function setup() {
-  new Canvas(windowWidth, windowHeight);
-  noStroke();
-
-  yVel = 0;
-  velocity = 0;
-  mass = 100;
-
-  accel = mass * 0.1;
-
-  groundHeight = 50;
-
-  sprite = new Sprite();
-  sprite.width = mass;
-  sprite.height = mass;
-  sprite.x = movement;
-  sprite.y = yVel;
+	new Canvas(windowWidth, windowHeight);
+	world.gravity.y = 15;
+	player = new Sprite(50, 0);
+	floor = new Sprite(25, floorHeight+600, windowWidth, 40, "static");
+  floor2 = new Sprite(10, floorHeight+250, 450, 40, "static");
+  floor3 = new Sprite(50, floorHeight+400, 225, 40, "static");
 }
-
 
 function draw() {
-  background(50);
-  //fill(255,0,0);
-
-  createGround();
-  //createPlayer();
-  playerControls();
+  background("white");
+  //clear();
+  
+  playerLeftAndRight();
+	camera.x = player.x;
 }
-
-
-
-function createGround() {
-  push();
-  fill("purple");
-  rect(0, windowHeight-50, windowWidth, groundHeight);
-  pop();
-}
-
-
-// function mousePressed() {
-//   push();
-//   yVel = 0;
-//   velocity = 0;
-//   mouseX===mouseX;
-//   pop();
+function playerLeftAndRight() {
+  if (kb.pressing("d")) {
+    //player.vel.y = 1;
+    player.vel.x = 3;
+  }
+  if (kb.pressing("a")) {
+    //player.vel.y = 1;
+    player.vel.x = -3;
+  }
+  if (kb.pressing("w")) {
+    player.vel.y = 30;
+    
+  }
+  console.log()
+//   if (player.vel.y = 30) {
 // }
-
-function playerControls() {
-  if (keyIsDown(68)) {
-    movement += 15;
-  }
-  if (keyIsDown(65)) {
-    movement -= 15;
-  }
-  if (keyIsDown(32)) {
-    yVel -= mass/2;
-    if (keyIsDown(65)) {
-      movement -= 17;
-    }
-    if (keyIsDown(68)) {
-      movement += 17;
-    }
-  }
 }
 
-
-
-
-
-
-// function createPlayer() {
-//   push();
-//   rectMode(CENTER);
-//   velocity += accel;
-//   yVel += velocity;
-//   push();
-//   sprite = new Sprite();
-//   sprite.width = mass;
-//   sprite.height = mass;
-//   sprite.x = movement;
-//   sprite.y = yVel;
-//   //texture(img);
-//   //rect(movement,yVel, mass,mass);
-//   pop();
-//   if (yVel > height - mass/2) {
-//     velocity *= -0.3;
-//     yVel = height - mass/2;
-//   }
-//   pop();
-// }
