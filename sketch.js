88f843322d74e91e9751602ea738c6416d1de052
Bosh <126823337+JoshMoreLikeBosh@users.noreply.github.com>
2023-05-03 15:19:44 -0600

    made the jump better and found the gimmick of creating platform under the player

--- sketch.js
+++ sketch.js
@@ -5,18 +5,15 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let player, floor, floor2, floor3, floor4;
+let player, floor, floor2, floor3, floor4, boxy;
 let floorHeight = 0;
+let jumpValue = -8;
 
 function setup() {
   new Canvas(windowWidth, windowHeight);
   world.gravity.y = 15;
   createPlayer();
-  //(10, height, width, verticle width, static keeps it in air)
-  floor = new Sprite(0, floorHeight+350, 200, 25, "dynamicd");
-  floor2 = new Sprite(0, floorHeight+400, 400, 50, "dynamica");
-  floor3 = new Sprite(0, floorHeight+450, 800, 50, "static");
-  floor4 = new Sprite(0, floorHeight+500, 800, 50, "static");
+  createSurface();
 } 
 
 function draw() {
@@ -36,12 +33,13 @@ function playerLeftAndRight() {
     player.vel.x = -3;
   }
   if (kb.released("w")) {
-    player.vel.y += 40;
-    
+    player.vel.y = jumpValue;
   }
-  console.log()
-//   if (player.vel.y = 30) {
-// }
+  if (kb.released("e")) {
+    boxy = new Sprite(100, 25, 150, 25, "s");
+    boxy.x = player.x + 0;
+    boxy.y = player.y + 50;
+  }
 }
 
 function createPlayer() {
@@ -52,7 +50,11 @@ function createPlayer() {
 }
 
 function createSurface () {
-  
+  floor = new Sprite(0, floorHeight+350, 200, 25, "d");
+  floor2 = new Sprite(0, floorHeight+400, 400, 50);
+  floor3 = new Sprite(0, floorHeight+450, 800, 50, "static");
+  floor4 = new Sprite(0, floorHeight+500, 800, 50, "static");
+  //boxy = new Sprite(0, 25, 25)
 }
 
 function playerDeath() {
