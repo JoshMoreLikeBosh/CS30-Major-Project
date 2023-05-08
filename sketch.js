42f800e8ddf776d3e4aa4b18405fcab1d9034dba
Bosh <126823337+JoshMoreLikeBosh@users.noreply.github.com>
2023-05-08 15:19:22 -0600

    refined controls

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ let floorWidth = 0;
 let floorX = 0;
 let floorY = 0;
 let jumpValue = -8;
+let boxyArray = [];
 
 function setup() {
   new Canvas(windowWidth, windowHeight);
@@ -26,33 +27,46 @@ function draw() {
   playerLeftAndRight();
 	camera.x = player.x;
 }
+
 function playerLeftAndRight() {
+  // move right
   if (kb.pressing("d")) {
     //player.vel.y = 1;
     player.vel.x = 3;
   }
+  // move left
   if (kb.pressing("a")) {
     //player.vel.y = 1;
     player.vel.x = -3;
   }
-  if (kb.released("w")) {
+  // ground pound
+  if (kb.presses("s")) {
+    player.vel.y = -jumpValue*2;
+    player.vel.x = 0;
+  }
+
+  // jump
+  if (kb.presses("w")) {
     player.vel.y = jumpValue;
   }
-  if (kb.released("e")) {
+  // create box under
+  
+  //Sprite(100, 25, 150, 25, "s");
+  if (kb.presses("e")) {
     boxy = new Sprite(100, 25, 150, 25, "s");
     boxy.x = player.x + 0;
     boxy.y = player.y + 50;
     boxy.color = "LightPink";
     boxy.stroke = "MediumVioletRed";
-    
+    boxyArray.push(boxy);
   }
-  if (kb.released("q")) {
-    boxy.remove();
-  }
-  if (kb.released("p")) {
-    reload();
+  if (kb.presses("q")) {
+    let toDestroy = boxyArray.pop();
+    toDestroy.remove();
   }
 }
+
+// push boxy into array and press delete key to remove from array
 
 function createPlayer() {
   player = new Sprite(50, 0);
